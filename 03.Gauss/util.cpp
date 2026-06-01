3cc71452e0da767dd45556d3a60e625f2e644646
Зимогляд Кирилл <kirill06052007@gmail.com>
2026-06-01 21:34:26 +0300

    Add util.cpp

--- 03.Gauss/util.cpp
+++ 03.Gauss/util.cpp
@@ -4,7 +4,7 @@
 
 #include "util.h"
 
-GaussMatrix load_csv_to_matrix(const char *filename)
+GaussMatrix load_csv_to_matrix(const char* filename)
 {
     std::vector<std::vector<double>> rcsv{};
     {
@@ -20,7 +20,15 @@ GaussMatrix load_csv_to_matrix(const char *filename)
         }
     }
 
-    return GaussMatrix(rcsv.size(), rcsv.begin()->size());
+    GaussMatrix matrix(rcsv.size(), rcsv.begin()->size());
+
+    for (size_t i = 0; i < rcsv.size(); ++i) {
+        for (size_t j = 0; j < rcsv[i].size(); ++j) {
+            matrix(i, j) = rcsv[i][j];
+        }
+    }
+
+    return matrix;
 }
 
 void print_matrix_as_csv(std::ostream& out, const GaussMatrix &matrix, int prec)
